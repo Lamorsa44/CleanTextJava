553d5123017e19119be31004f9394128be52a6ed
uebito123 <uebito123@gmail.com>
2026-02-03 23:21:06 -0600

    🧊

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,20 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.UnsupportedFlavorException;
 
 void main() {
-    System.out.println("""
+    String info = """
             1. Check clipboard
             2. Remove horizontal lines
             3. Leave only one line between paragraphs
             4. Remove last paragraph
+            5. Remove list spacing
+            6. Remove listing
+            7. Remove listing from titles
             0. Exit
-            """);
+            """;
+    System.out.println(info);
 
+    System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+    System.out.println(System.getProperty("stdin.encoding"));
     Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
     boolean running = true;
 
@@ -24,6 +30,10 @@ void main() {
                 case "2" -> removeHorizontalLines(clipboard);
                 case "3" -> oneLineBetweenParagraphs(clipboard);
                 case "4" -> removeLastParagraph(clipboard);
+                case "5" -> removeListSpacing(clipboard);
+                case "6" -> removeListing(clipboard);
+                case "7" -> removeListingFromTitles(clipboard);
+                default -> System.out.println(info);
             }
         } catch (UnsupportedFlavorException e) {
             System.out.println("Unsupported clipboard data flavor");
@@ -33,27 +43,50 @@ void main() {
     }
 }
 
+private void removeListingFromTitles(Clipboard clipboard) throws IOException, UnsupportedFlavorException {
+    String data = getStringFromClipBoard(clipboard)
+            .replaceAll("(?m)(^#+)(\\s+\\d+\\.\\s+)", "$1 ")
+            .replaceAll("(?m)(^#+)(\\s+-\\s+)", "$1 ");
+    printAndCopy(clipboard, data);
+}
+
+private void removeListing(Clipboard clipboard) throws IOException, UnsupportedFlavorException {
+    String data = getStringFromClipBoard(clipboard)
+            .replaceAll("(?m)(^\\d+\\.\\s+)", "")
+            .replaceAll("(?m)(^\\s*-\\s+)", "");
+    printAndCopy(clipboard, data);
+}
+
+private void removeListSpacing(Clipboard clipboard) throws IOException, UnsupportedFlavorException {
+    String data = getStringFromClipBoard(clipboard)
+            .replaceAll("(?m)(^\\d+\\.\\s+.*)(\r?\n)+", "$1")
+            .replaceAll("(?m)(^\\s*-\\s+.*)(\r?\n)+", "$1\n");
+    printAndCopy(clipboard, data);
+}
+
 private void removeLastParagraph(Clipboard clipboard) throws IOException, UnsupportedFlavorException {
-  String data = getStringFromClipBoard(clipboard);
-  String substring = data.substring(0, data.stripTrailing().lastIndexOf("\n\n"));
-  System.out.println(substring);
-  clipboard.setContents(new StringSelection(substring), null);
+    String data = getStringFromClipBoard(clipboard);
+    String substring = data.substring(0, data.stripTrailing().lastIndexOf("\n\n"));
+    printAndCopy(clipboard, substring);
 }
 
 private void oneLineBetweenParagraphs(Clipboard clipboard) throws IOException, UnsupportedFlavorException {
     String data = getStringFromClipBoard(clipboard)
             .replaceAll("(\\r?\\n\\s*){2,}", "\n\n");
-    System.out.println(data);
-    clipboard.setContents(new StringSelection(data), null);
+    printAndCopy(clipboard, data);
 }
 
 
 private void removeHorizontalLines(Clipboard clipboard) throws IOException, UnsupportedFlavorException {
-  String data = getStringFromClipBoard(clipboard).replaceAll("(?m)^-+$", "");
-  System.out.println(data);
-  clipboard.setContents(new StringSelection(data), null);
+    String data = getStringFromClipBoard(clipboard).replaceAll("(?m)^-+$", "");
+    printAndCopy(clipboard, data);
 }
 
 private static String getStringFromClipBoard(Clipboard clipboard) throws UnsupportedFlavorException, IOException {
-  return (String) clipboard.getData(DataFlavor.stringFlavor);
+    return (String) clipboard.getData(DataFlavor.stringFlavor);
+}
+
+private static void printAndCopy(Clipboard clipboard, String data) {
+    System.out.println(data);
+    clipboard.setContents(new StringSelection(data), null);
 }
